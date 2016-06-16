62ca6e870620acbb98abba2e405322258e5d9b12
dochonglo <ismaelbarry777@hotmail.com>
2016-06-16 15:49:21 -0400

    OOP Cards Simple View Lab is Complete (All Tests Passed).

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,6 +3,9 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
 
 @end
 
@@ -20,6 +23,38 @@
                                                     rank:@"8"];
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
+}
+
+- (IBAction)threeOfSpadesButtonDidTouch {
+
+    self.topLabel.text = @"♠️3";
+    self.middleLabel.text = @"♠️3";
+    self.bottomLabel.text = @"♠️3";
+
+}
+
+- (IBAction)fourOfClubsButtonDidTouch {
+
+    self.topLabel.text = @"♣️4";
+    self.middleLabel.text = @"♣️4";
+    self.bottomLabel.text = @"♣️4";
+
+}
+
+- (IBAction)eightOfDiamondsButtonDidTouch {
+
+    self.topLabel.text = @"♦️8";
+    self.middleLabel.text = @"♦️8";
+    self.bottomLabel.text = @"♦️8";
+
+}
+
+- (IBAction)tenOfHeartsButtonDidTouch {
+
+    self.topLabel.text = @"♥️10";
+    self.middleLabel.text = @"♥️10";
+    self.bottomLabel.text = @"♥️10";
+
 }
 
 @end
