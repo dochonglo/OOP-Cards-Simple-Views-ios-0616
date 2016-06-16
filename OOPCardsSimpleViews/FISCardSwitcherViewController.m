d98e57d478490a7b8017252207d26788f955b9a8
dochonglo <ismaelbarry777@hotmail.com>
2016-06-16 16:43:38 -0400

    Updated code to eliminate string literals in IBAction Methods.

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -28,32 +28,44 @@
 - (IBAction)threeOfSpadesButtonDidTouch {
 
     self.topLabel.text = @"♠️3";
+    // self.topLabel.text = self.threeOfSpades.cardLabel;
     self.middleLabel.text = @"♠️3";
+    // self.topLabel.text = self.threeOfSpades.cardLabel;
     self.bottomLabel.text = @"♠️3";
+    // self.topLabel.text = self.threeOfSpades.cardLabel;
 
 }
 
 - (IBAction)fourOfClubsButtonDidTouch {
 
     self.topLabel.text = @"♣️4";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
     self.middleLabel.text = @"♣️4";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
     self.bottomLabel.text = @"♣️4";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
 
 }
 
 - (IBAction)eightOfDiamondsButtonDidTouch {
 
     self.topLabel.text = @"♦️8";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
     self.middleLabel.text = @"♦️8";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
     self.bottomLabel.text = @"♦️8";
+    // self.topLabel.text = self.fourOfClubs.cardLabel;
 
 }
 
 - (IBAction)tenOfHeartsButtonDidTouch {
 
     self.topLabel.text = @"♥️10";
+    // self.topLabel.text = self.tenOfHearts.cardLabel;
     self.middleLabel.text = @"♥️10";
+    // self.topLabel.text = self.tenOfHearts.cardLabel;
     self.bottomLabel.text = @"♥️10";
+    // self.topLabel.text = self.tenOfHearts.cardLabel;
 
 }
 
